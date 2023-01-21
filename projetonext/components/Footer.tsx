3e61e48a9ea98e94ef367b16c6e2f97f1593f5d9
Mayza <mayza.santos@outlook.com.br>
2023-01-21 17:16:15 -0300

    fist commit

---
+++ projetonext/components/Footer.tsx
@@ -0,0 +1,11 @@
+import { StyledFooter } from "@/styles/StyledFooter"
+
+export const Footer = ()=>{
+    return(
+        <StyledFooter>
+
+            <h3>TXT inteprice</h3>
+        </StyledFooter>
+    )
+}
+
---
+++ projetonext/components/Header.tsx
@@ -0,0 +1,10 @@
+
+
+export const Header = ()=>{
+return(
+    <>
+    <h3>TXT Interpase</h3>
+    <button>Home</button>
+    </>
+)
+}
